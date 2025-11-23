bb223e9fe4a6b6f1addc68a6d3cf8daea194a472
Zoltan Sasvari <sasvarizoli@me.com>
2025-11-23 16:08:38 +0100

    initial commit

---
+++ src/main/kotlin/com/functionaldude/paperless_customGPT/PaperlessCustomGptApplication.kt
@@ -0,0 +1,11 @@
+package com.functionaldude.paperless_customGPT
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PaperlessCustomGptApplication
+
+fun main(args: Array<String>) {
+	runApplication<PaperlessCustomGptApplication>(*args)
+}
---
+++ src/test/kotlin/com/functionaldude/paperless_customGPT/PaperlessCustomGptApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.functionaldude.paperless_customGPT
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PaperlessCustomGptApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
